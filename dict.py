343402f52c0a7ec3f08e1f21d1f048fc6f420f73
pyohanes0608 <143081320+pyohanes0608@users.noreply.github.com>
2023-11-06 14:26:54 +0700

    Update dict.py

--- dict.py
+++ dict.py
@@ -1,6 +1,10 @@
 meme_dict = {
             "CRINGE": "Something very strange or embarrassing",
             "LOL": "A common response to something funny",
+            "ROFL": "typically used to respond for a great joke",
+            "SHEESH": "used to express disbelief or exasperation",
+            "CREEPY": "causing an unpleasant feeling of fear or unease",
+            "AGGRO": "aggressive, violent behavior",
             }
 
 word = input("Enter the word that you don't understand!  (All capital!): ")
